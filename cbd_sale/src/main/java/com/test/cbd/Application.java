c698e8544882185241be1c8b8b3012ee18e6b27f
zxj <961310225@qq.com>
2018-11-16 18:12:35 +0800

    es

--- cbd_sale/src/main/java/com/test/cbd/Application.java
+++ cbd_sale/src/main/java/com/test/cbd/Application.java
@@ -4,15 +4,17 @@ import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.amqp.core.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})//ElasticsearchDataAutoConfiguration.class
 @EnableDubboConfiguration
 public class Application {
 
     public static void main(String[] args) throws Exception {
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
         SpringApplication.run(Application.class, args);
     }
 }
